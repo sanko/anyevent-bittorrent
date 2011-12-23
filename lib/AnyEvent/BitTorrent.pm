880b6446b043658e1c20f37aeac01e0d8033abde
Sanko Robinson <sanko@cpan.org>
2011-12-23 01:26:07 -0500

    Fix stable/unstable peerid generator

--- lib/AnyEvent/BitTorrent.pm
+++ lib/AnyEvent/BitTorrent.pm
@@ -80,7 +80,7 @@ has peerid => (
             (sprintf(
                  '-AB%01d%02d%1s-%7s%-5s',
                  ($AnyEvent::BitTorrent::VERSION =~ m[^v(\d+)\.(\d+)]),
-                 ($AnyEvent::BitTorrent::VERSION =~ m[\.[^\d]+$] ? 'U' : 'S'),
+                 ($AnyEvent::BitTorrent::VERSION =~ m[\_] ? 'U' : 'S'),
                  (join '',
                   map {
                       ['A' .. 'Z', 'a' .. 'z', 0 .. 9, qw[- . _ ~]]
