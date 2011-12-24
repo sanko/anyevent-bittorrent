8930180a0852a1c7160d7f2872ef574d90f7ec2f
Sanko Robinson <sanko@cpan.org>
2011-12-23 19:41:47 -0500

    v1.0.3 headed for PAUSE

--- lib/AnyEvent/BitTorrent.pm
+++ lib/AnyEvent/BitTorrent.pm
@@ -1,5 +1,5 @@
 package AnyEvent::BitTorrent;
-{ $AnyEvent::BitTorrent::VERSION = 'v0.1.2' }
+{ $AnyEvent::BitTorrent::VERSION = 'v0.1.3' }
 use AnyEvent;
 use AnyEvent::Handle;
 use AnyEvent::Socket;
